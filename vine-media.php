b2cd430a2de2e34233138fc297366cbd436c5a9c
Bryan Orozco <bryanorozcoweb@gmail.com>
2018-01-15 23:54:38 -0800

    First commit

---
+++ vine-media.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Vine Media
+Plugin URI:  https://developer.wordpress.org/plugins/the-basics/
+Description: Media manager for the Vine Theme
+Version:     0.0.1
+Author:      Bryan Orozco
+Author URI:  https://github.com/borozcod
+Text Domain: vine_media
+Domain Path: /languages
+License:     GPL3
+
+Vine Media is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Vine Media is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vine Media. If not, see https://www.gnu.org/licenses/gpl.html.
+*/
